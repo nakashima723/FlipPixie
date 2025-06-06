06772f4decafa6ce773d691b6fbb362ba68eb0d2
Nakashima723 <110010061+nakashima723@users.noreply.github.com>
2025-06-06 16:34:58 +0900

    Add newline at end of contents.js

--- js/popup.js
+++ js/popup.js
@@ -5,16 +5,20 @@ $(function(){
       $('html,body').height(height);
     }
 
-    var FlipX = items.FlipX,
-        FlipY = items.FlipY,
-        Rotate = items.Rotate,
-        OK = true;
+      var FlipX = items.FlipX,
+          FlipY = items.FlipY,
+          Rotate = items.Rotate,
+          Brightness = items.Brightness,
+          Contrast = items.Contrast,
+          OK = true;
 
     if(FlipX === undefined){ FlipX = "off"; }
     if(FlipY === undefined){ FlipY = "off"; }
-    if(Rotate === undefined){ Rotate = "0"; }
+      if(Rotate === undefined){ Rotate = "0"; }
+      if(Brightness === undefined){ Brightness = 0; }
+      if(Contrast === undefined){ Contrast = 0; }
 
-    var loadButton = function(){
+      var loadButton = function(){
       if(FlipX === "on"){
         $("#flipX").css({'color':'#fff','background-color':'#00aced'});
       } else {
@@ -27,8 +31,10 @@ $(function(){
         $("#flipY").css({'color':'#666','background-color':'#fff'});
       }
 
-      $("#rotate").val(Rotate);
-    };	
+        $("#rotate").val(Rotate);
+        $("#brightness").val(Brightness);
+        $("#contrast").val(Contrast);
+      };
 
     loadButton();	
 
@@ -58,16 +64,43 @@ $(function(){
       chrome.storage.sync.set(option);
     };
 
-    var setRotate = function(){	
-      var Rotate = $("#rotate").children(':selected').val(); 	 
-      var option = {};
-      option.Rotate = Rotate;
-      chrome.storage.sync.set(option);
-    };
+      var setRotate = function(){
+        var Rotate = $("#rotate").children(':selected').val();
+        var option = {};
+        option.Rotate = Rotate;
+        chrome.storage.sync.set(option);
+      };
 
-    $("#flipX").click(function(){ setButtonX(); });
-    $("#flipY").click(function(){ setButtonY(); });
-    $("#rotate").change(function(){ setRotate(); });
+      var notifyContent = function(){
+        chrome.tabs.query({ active: true, currentWindow: true }, function(tabs){
+          if (tabs[0]) {
+            chrome.tabs.sendMessage(tabs[0].id, { type: 'refresh' });
+          }
+        });
+      };
+
+      var setBrightness = function(){
+        var val = parseInt($("#brightness").val(), 10) || 0;
+        chrome.storage.sync.set({ Brightness: val }, notifyContent);
+      };
+
+      var setContrast = function(){
+        var val = parseInt($("#contrast").val(), 10) || 0;
+        chrome.storage.sync.set({ Contrast: val }, notifyContent);
+      };
+
+      var resetBC = function(){
+        $("#brightness").val(0);
+        $("#contrast").val(0);
+        chrome.storage.sync.set({ Brightness: 0, Contrast: 0 }, notifyContent);
+      };
+
+      $("#flipX").click(function(){ setButtonX(); });
+      $("#flipY").click(function(){ setButtonY(); });
+      $("#rotate").change(function(){ setRotate(); });
+      $("#brightness").on('input change', function(){ setBrightness(); });
+      $("#contrast").on('input change', function(){ setContrast(); });
+      $("#bc-reset").click(function(){ resetBC(); });
 
     // ジョグダイアル → select を動かすための連携
     if (typeof window.jogDialAngleHook === 'function') {
